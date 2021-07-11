02334ed62a91a75acee352a912acbf83d759143e
kamrul1717 <kamrulhassan1361@gmail.com>
2021-07-12 00:26:36 +0600

    crawled multi page amazon book and other info

--- crawl_amazon/spiders/amazon.py
+++ crawl_amazon/spiders/amazon.py
@@ -1,12 +1,13 @@
 import scrapy
 from ..items import CrawlAmazonItem
 
-
 class AmazonSpider(scrapy.Spider):
     name = 'amazon'
     start_urls = [
         'https://www.amazon.com/s?k=novel&i=stripbooks-intl-ship&ref=nb_sb_noss'
     ]
+
+    page_number = 2
 
     def parse(self, response):
         items = CrawlAmazonItem()
@@ -20,3 +21,8 @@ class AmazonSpider(scrapy.Spider):
         items['book_price'] = book_price
         items['book_image_link'] = book_image_link
         yield items
+
+        next_page = 'https://www.amazon.com/s?k=novel&i=stripbooks-intl-ship&page='+str(AmazonSpider.page_number)+'&qid=1626024295&ref=sr_pg_1'
+        if AmazonSpider.page_number <= 5:
+            AmazonSpider.page_number += 1
+            yield response.follow(next_page, callback = self.parse)
